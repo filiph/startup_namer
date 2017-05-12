be98a606d0e8d26a743aa0fe3d49d51cf49311a3
Filip Hracek <filip.hracek@gmail.com>
2017-05-12 14:36:14 -0700

    Initialize the PWA client

--- web/main.dart
+++ web/main.dart
@@ -1,10 +1,12 @@
 // Copyright (c) 2017, filiph. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'package:angular2/angular2.dart';
 import 'package:angular2/platform/browser.dart';
-
+import 'package:pwa/client.dart' as pwa;
 import 'package:startup_namer/app_component.dart';
 
 void main() {
-  bootstrap(AppComponent);
+  bootstrap(
+      AppComponent, [new Provider(pwa.Client, useValue: new pwa.Client())]);
 }
