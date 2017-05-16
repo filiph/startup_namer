633b1f60410f96f5e5f3fe8b8c721f2c4ee29775
Kathy Walrath <kathyw@google.com>
2017-05-16 12:37:01 -0700

    move directives, import angular2.dart

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:angular2/core.dart';
+import 'package:angular2/angular2.dart';
 import 'package:angular_components/angular_components.dart';
 
 import 'todo_list/todo_list_component.dart';
@@ -14,7 +14,7 @@ import 'todo_list/todo_list_component.dart';
   selector: 'my-app',
   styleUrls: const ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: const [materialDirectives, TodoListComponent],
+  directives: const [CORE_DIRECTIVES, materialDirectives, TodoListComponent],
   providers: const [materialProviders],
 )
 class AppComponent {
