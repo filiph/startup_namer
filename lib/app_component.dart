1a90f51321c7904b4f910bcd91bcad6d1d73f354
Kathy Walrath <kathyw@google.com>
2017-05-16 12:30:56 -0700

    move directives, import angular2.dart

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:angular2/core.dart';
+import 'package:angular2/angular2.dart';
 import 'package:angular_components/angular_components.dart';
 import 'package:english_words/english_words.dart';
 import 'package:startup_namer/local_storage_service.dart';
@@ -14,7 +14,7 @@ import 'package:startup_namer/local_storage_service.dart';
   selector: 'my-app',
   styleUrls: const ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: const [materialDirectives],
+  directives: const [CORE_DIRECTIVES, materialDirectives],
   providers: const [materialProviders, LocalStorageService],
 )
 class AppComponent implements OnInit {
